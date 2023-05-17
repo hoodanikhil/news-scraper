b8ee05a79452df093c73b5217bf0b97bb2746a95
hoodanikhil <43134204+hoodanikhil@users.noreply.github.com>
2023-05-17 20:00:01 +0530

    Update script.js

--- api/static/script.js
+++ api/static/script.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
     $("#myForm").submit(function(event) {
         event.preventDefault();
         const inputText = $("#myTextField").val();
+        const numArticles = $("#numArticlesField").val();
         $("#loadingMessage").show(); // Show the loading message
-        $.post("/button", { text: inputText }, function(data) {
+        $.post("/button", { text: inputText, num_articles: numArticles }, function(data) {
             let articles = data.result.map(article => {
                 let bodyText = article.description ? article.description : "";
                 let dateString = article.date_publish? (new Date(article.date_publish)).toLocaleDateString('en-US', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' }) : "";
@@ -16,7 +17,7 @@ $(document).ready(function() {
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title"><a href="${article.article_url}">${article.headline} - ${article.source}</a></h5>
+                                <h5 class="card-title"><a href="${article.article_url}" target="_blank">${article.headline} - ${article.source}</a></h5>
                                 <p class="card-title"><small class="text-muted">${dateString}</small></p>
                                 <p class="card-text">${bodyText}</p>
                             </div>
